5e8b6d41714c309732c350f503e4848c9361414a
Justin Harrison <justin@matthin.com>
2015-07-26 19:49:19 -0400

    Add a few extra forth words

--- include/forthan/Compiler.h
+++ include/forthan/Compiler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <iostream>
 #include <stack>
 #include <string>
 #include <unordered_map>
@@ -24,6 +25,15 @@ private:
     {"-", std::bind(&Compiler::subtract, this)},
     {"*", std::bind(&Compiler::multiply, this)},
     {"/", std::bind(&Compiler::divide, this)},
+    {".", std::bind(&Compiler::print, this)},
+    {"CR", std::bind(&Compiler::carriageReturn, this)},
+    {"ABS", std::bind(&Compiler::absoluteValue, this)},
+
+    // Bitwise Operators
+    {"AND", std::bind(&Compiler::bitwiseAND, this)},
+    {"OR", std::bind(&Compiler::bitwiseOR, this)},
+    {"XOR", std::bind(&Compiler::bitwiseXOR, this)},
+    {"NOT", std::bind(&Compiler::bitwiseNOT, this)}
   };
 
   void add() {
@@ -48,6 +58,41 @@ private:
     const auto first = stackPopTop();
     stack.push(
       first / second
+    );
+  }
+  void print() {
+    std::cout << stackPopTop();
+  }
+  void carriageReturn() {
+    std::cout << '\r';
+  }
+  void absoluteValue() {
+    stack.push(
+      std::abs(stackPopTop())
+    );
+  }
+
+  /*
+   * Bitwise Operators
+   */
+  void bitwiseAND() {
+    stack.push(
+      stackPopTop() & stackPopTop()
+    );
+  }
+  void bitwiseOR() {
+    stack.push(
+      stackPopTop() | stackPopTop()
+    );
+  }
+  void bitwiseXOR() {
+    stack.push(
+      stackPopTop() ^ stackPopTop()
+    );
+  }
+  void bitwiseNOT() {
+    stack.push(
+      ~stackPopTop()
     );
   }
 };
--- test/Compiler.cc
+++ test/Compiler.cc
@@ -7,3 +7,28 @@ TEST_CASE("Compiles all basic math operators") {
   REQUIRE(compiler.stack.top() == 13);
 }
 
+TEST_CASE("Implements absolute value") {
+  ft::Compiler compiler("-47 ABS");
+  REQUIRE(compiler.stack.top() == 47);
+}
+
+TEST_CASE("Implements bitwise AND") {
+  ft::Compiler compiler("10 8 AND");
+  REQUIRE(compiler.stack.top() == 8);
+}
+
+TEST_CASE("Implements bitwise OR") {
+  ft::Compiler compiler("7 46 OR");
+  REQUIRE(compiler.stack.top() == 47);
+}
+
+TEST_CASE("Implements bitwise XOR") {
+  ft::Compiler compiler("87 6 XOR");
+  REQUIRE(compiler.stack.top() == 81);
+}
+
+TEST_CASE("Implements bitwise NOT") {
+  ft::Compiler compiler("9 NOT");
+  REQUIRE(compiler.stack.top() == -10);
+}
+
